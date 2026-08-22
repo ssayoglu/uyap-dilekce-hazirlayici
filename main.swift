9e91ed9418a6fac7bd318378a69df660b7c33291
L. Serkan SAYOĞLU <serkan@SS-Hukuk-Mini.local>
2026-08-22 15:09:29 +0300

    feat(autoupdate): add automatic background git update on startup and manual check under Application menu (Cmd+U)

--- main.swift
+++ main.swift
@@ -12,6 +12,12 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
         setupMainMenu()
 
         let fileManager = FileManager.default
+        let homeDir = NSHomeDirectory()
+        let targetDir = "\(homeDir)/.dilekce-hazirlayici"
+
+        // 1. Arka Planda Otomatik GitHub Güncellemesi (Her açılışta sessizce son sürümü çeker)
+        checkForUpdatesInBackground(targetDir: targetDir)
+
         let pythonPaths = [
             "/opt/homebrew/bin/python3",
             "/usr/local/bin/python3",
@@ -19,17 +25,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
         ]
         let pythonBinary = pythonPaths.first(where: { fileManager.fileExists(atPath: $0) }) ?? "/usr/bin/python3"
 
-        let homeDir = NSHomeDirectory()
         let possibleServerPaths = [
+            "\(targetDir)/server.py",
             Bundle.main.path(forResource: "server", ofType: "py"),
-            "\(homeDir)/.dilekce-hazirlayici/server.py",
             "\(Bundle.main.bundlePath)/Contents/Resources/server.py",
             "\(Bundle.main.bundlePath)/../server.py",
             "\(FileManager.default.currentDirectoryPath)/server.py",
             "/Users/serkan/Documents/DilekceOlusturucu/server.py"
         ].compactMap { $0 }
 
-        let serverScript = possibleServerPaths.first(where: { fileManager.fileExists(atPath: $0) }) ?? "\(homeDir)/.dilekce-hazirlayici/server.py"
+        let serverScript = possibleServerPaths.first(where: { fileManager.fileExists(atPath: $0) }) ?? "\(targetDir)/server.py"
 
         // Kill any previous hanging instance on port 5678
         let killTask = Process()
@@ -45,10 +50,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
         try? task.run()
         self.pythonProcess = task
 
-        // 1. Status bar (Menü çubuğu simgesi)
+        // 2. Status bar (Menü çubuğu simgesi)
         setupStatusItem()
 
-        // 2. Window
+        // 3. Window
         let rect = NSRect(x: 0, y: 0, width: 960, height: 880)
         window = NSWindow(
             contentRect: rect,
@@ -74,14 +79,42 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
 
         // Load URL
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-            if let url = URL(string: "http://127.0.0.1:5678/?t=\(Date().timeIntervalSince1970)") {
-                var req = URLRequest(url: url)
-                req.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
-                self.webView.load(req)
-            }
+            self.loadWebPage()
         }
 
         showAppWindow()
+    }
+
+    func loadWebPage() {
+        if let url = URL(string: "http://127.0.0.1:5678/?t=\(Date().timeIntervalSince1970)") {
+            var req = URLRequest(url: url)
+            req.cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+            self.webView.load(req)
+        }
+    }
+
+    // GitHub Otomatik Güncelleme Motoru (Arka planda sessizce çeker)
+    func checkForUpdatesInBackground(targetDir: String) {
+        DispatchQueue.global(qos: .background).async {
+            let fm = FileManager.default
+            if fm.fileExists(atPath: "\(targetDir)/.git") {
+                let gitTask = Process()
+                gitTask.launchPath = "/usr/bin/git"
+                gitTask.currentDirectoryPath = targetDir
+                gitTask.arguments = ["pull", "--quiet"]
+                try? gitTask.run()
+                gitTask.waitUntilExit()
+
+                if gitTask.terminationStatus == 0 {
+                    // Güncelleme çekildiyse Resources/server.py'yi de senkronize et
+                    let appResourcesServer = "\(Bundle.main.bundlePath)/Contents/Resources/server.py"
+                    if fm.fileExists(atPath: appResourcesServer) && fm.fileExists(atPath: "\(targetDir)/server.py") {
+                        try? fm.removeItem(atPath: appResourcesServer)
+                        try? fm.copyItem(atPath: "\(targetDir)/server.py", toPath: appResourcesServer)
+                    }
+                }
+            }
+        }
     }
 
     func setupMainMenu() {
@@ -93,6 +126,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
         let appMenu = NSMenu()
         appMenuItem.submenu = appMenu
         appMenu.addItem(withTitle: "Hakkında", action: #selector(NSApplication.orderFrontStandardAboutPanel(_:)), keyEquivalent: "")
+        appMenu.addItem(withTitle: "Güncellemeleri Denetle", action: #selector(manualUpdateCheck), keyEquivalent: "u")
         appMenu.addItem(NSMenuItem.separator())
         appMenu.addItem(withTitle: "Gizle", action: #selector(NSApplication.hide(_:)), keyEquivalent: "h")
         let hideOthersItem = NSMenuItem(title: "Diğerlerini Gizle", action: #selector(NSApplication.hideOtherApplications(_:)), keyEquivalent: "h")
@@ -117,16 +151,41 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate, WKNavigati
         editMenu.addItem(withTitle: "Yapıştır", action: #selector(NSText.paste(_:)), keyEquivalent: "v")
         editMenu.addItem(withTitle: "Tümünü Seç", action: #selector(NSText.selectAll(_:)), keyEquivalent: "a")
 
-        // 3. Pencere Menüsü (Window Menu - ⌘W, ⌘M)
+        // 3. Pencere Menüsü (Window Menu - ⌘W, ⌘M, ⌘R)
         let windowMenuItem = NSMenuItem()
         mainMenu.addItem(windowMenuItem)
         let windowMenu = NSMenu(title: "Pencere")
         windowMenuItem.submenu = windowMenu
+        windowMenu.addItem(withTitle: "Yenile", action: #selector(reloadPage), keyEquivalent: "r")
         windowMenu.addItem(withTitle: "Pencereyi Kapat", action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w")
         windowMenu.addItem(withTitle: "Simge Durumuna Küçült", action: #selector(NSWindow.performMiniaturize(_:)), keyEquivalent: "m")
         windowMenu.addItem(withTitle: "Yakınlaştır", action: #selector(NSWindow.performZoom(_:)), keyEquivalent: "")
 
         NSApplication.shared.mainMenu = mainMenu
+    }
+
+    @objc func reloadPage() {
+        loadWebPage()
+    }
+
+    @objc func manualUpdateCheck() {
+        let homeDir = NSHomeDirectory()
+        let targetDir = "\(homeDir)/.dilekce-hazirlayici"
+        
+        let gitTask = Process()
+        gitTask.launchPath = "/usr/bin/git"
+        gitTask.currentDirectoryPath = targetDir
+        gitTask.arguments = ["pull"]
+        try? gitTask.run()
+        gitTask.waitUntilExit()
+
+        reloadPage()
+        
+        let alert = NSAlert()
+        alert.messageText = "Güncelleme Kontrolü"
+        alert.informativeText = "Uygulama GitHub üzerinden en son sürüme güncellendi ve yenilendi."
+        alert.alertStyle = .informational
+        alert.runModal()
     }
 
     func setupStatusItem() {
